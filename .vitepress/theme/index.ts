5acfdba4a13350248221b5bac6fd64f92f5db5bd
SHgzg <1754737900@qq.com>
2025-07-18 01:09:16 +0800

    repress replace

--- .vitepress/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import Context from './components/Context.vue'
-
-export default {
-  extends: DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('Context', Context)
-  }
-}
